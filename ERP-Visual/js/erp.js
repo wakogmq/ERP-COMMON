61d435839d72ad2f27f11a29df40e0126085a5b8
wakogmq <manu12gmq@gmail.com>
2022-11-17 13:33:10 +0100

    Update erp.js

--- ERP-Visual/js/erp.js
+++ ERP-Visual/js/erp.js
@@ -3,20 +3,29 @@
 toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
+    let initialImg = document.getElementById("imgdrop");
+    let srcTest = initialImg.includes('/ERP-Visual/images/dropclose.png');
+    let newImg = {
+      'true':'/ERP-Visual/images/dropopen.png', 
+      'false':'/ERP-Visual/images/dropclose.png'}[srcTest];
+  
+    return newImg;
+}
   
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
+    document.getElementById("imgdrop").src = myFunction();
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
-          var imgReplace = document.getElementsByClassName("imgdrop")[0];
-          imgReplace.src = "/ERP-Visual/images/dropopen.png";
+          
           openDropdown.classList.remove('show');
         }
       }
     }
+    
   }
+
