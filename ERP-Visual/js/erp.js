52335f84446a7b4a6ffcb92a44bbe450bcd73036
josema1334 <josema1334@gmail.com>
2022-11-22 09:03:49 +0100

    Update erp.js

--- ERP-Visual/js/erp.js
+++ ERP-Visual/js/erp.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
@@ -29,3 +30,16 @@ function myFunction() {
     
   }
 
+=======
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+>>>>>>> Stashed changes
