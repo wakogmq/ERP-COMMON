39e8612585cd027811269553aaa19ca02e9e7140
josema1334 <josema1334@gmail.com>
2023-02-07 09:14:25 +0100

    links

--- ERP-Visual/php/clientes.php
+++ ERP-Visual/php/clientes.php
@@ -9,30 +9,30 @@
     <script src="/js/erp.js"></script> 
 </head>
 <body class="clientes">
-    <header>
+<header>
         <nav>
         <ul class="nav flex-column">
             <li class="nav-item">
-              <img src="/ERP-Visual/images/casa (2).png" alt=""><a class="nav-link active" href="home.html">Home</a>
+              <img src="/ERP-Visual/images/casa (2).png" alt=""><a class="nav-link active" href="home.php">Home</a>
             </li>
             <li class="nav-item" id="clientes">
-              <img src="/ERP-Visual/images/clientes.png" alt=""><a class="nav-link" href="clientes.html">Clientes</a>
+              <img src="/ERP-Visual/images/clientes.png" alt=""><a class="nav-link" href="clientes.php">Clientes</a>
             </li>
             <li class="nav-item">
-              <img src="/ERP-Visual/images/ventas.png" alt=""><a class="nav-link" href="ventas.html">Ventas</a>
+              <img src="/ERP-Visual/images/ventas.png" alt=""><a class="nav-link" href="ventas.php">Ventas</a>
             </li>
             <li class="nav-item" >
-                <img src="/ERP-Visual/images/envios.png" alt="">   <a class="nav-link disabled" href="envios.html">Envios</a>
+                <img src="/ERP-Visual/images/envios.png" alt="">   <a class="nav-link disabled" href="envios.php">Envios</a>
             </li>
             <li class="nav-item">
                 
-                <img src="/ERP-Visual/images/compras.png" alt="">  <a class="nav-link disabled" href="compras.html">Compras</a>
+                <img src="/ERP-Visual/images/compras.png" alt="">  <a class="nav-link disabled" href="compras.php">Compras</a>
               </li>
               <li class="nav-item">
-                <img src="/ERP-Visual/images/almacen.png" alt=""><a class="nav-link disabled" href="almacen.html">Almacen</a>
+                <img src="/ERP-Visual/images/almacen.png" alt=""><a class="nav-link disabled" href="almacen.php">Almacen</a>
               </li>
               <li class="nav-item">
-               <img src="/ERP-Visual/images/proveedores.png" alt=""> <a class="nav-link disabled" href="proveedores.html">Proovedores</a>
+               <img src="/ERP-Visual/images/proveedores.png" alt=""> <a class="nav-link disabled" href="proveedores.php">Proovedores</a>
               </li>
           </ul>
         </nav>
