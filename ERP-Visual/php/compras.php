0c545d6a4bd1d4cb63b7b8490e0b991daf504033
josema1334 <josema1334@gmail.com>
2023-02-09 12:18:37 +0100

    Update compras.php

--- ERP-Visual/php/compras.php
+++ ERP-Visual/php/compras.php
@@ -50,68 +50,7 @@
       <input type="button" value="Agregar" id="agregar">
       <input type="button" value="Rutas" id="rutas">
     </div>
-      <section class="tabla">
-      <table>
-        <tr>
-          <th>COD</th>
-          <th>Nombre</th>
-          <th>Cantidad</th>
-          <th>PVP Unidad € </th>
-          <th>PVP Total €</th>
-        </tr>
-  
-        <tr>
-          <td>1</td>
-          <td>NVIDIA RTX 2080</td>
-          <td>4</td>
-          <td>800</td>
-          <td>3200</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Intel i7 10700K</td>
-          <td>6</td>
-          <td>360</td>
-          <td>2160</td>
-        </tr>
-          <tr>
-          <td>3</td>
-          <td>Memoria RAM 16GB</td>
-          <td>12</td>
-          <td>130
-          </td>
-          <td>1560</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>Intel i5 10500K</td>
-          <td>3</td>
-          <td>250</th>
-          <td>750</td>
-        </tr>
-  
-        <tr>
-          <td>5</td>
-          <td>Ventilador 120mm</td>
-          <td>7</td>
-          <td>25</th>
-          <td>175</td>
-        </tr>
-  
-        <tr>
-          <td>6</td>
-          <td>Fuente Alimentacion 600W</td>
-          <td>4</td>
-          <td>120</th>
-          <td>480</td>
-        </tr>
-  
       
-        
-  
-        
-      </table>
-    </section>
 
 </body>
 </html>
